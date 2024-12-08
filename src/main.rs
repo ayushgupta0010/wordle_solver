b6aac10544fff6a94c0b79430148a946b4c48d6d
ayushgupta0010 <ayushgupta0010@gmail.com>
2024-12-07 21:20:05 -0500

    Add foundation code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const GAMES: &str = include_str!("../answers.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let w = wordle_solver::Wordle::new();
+    for answer in GAMES.split_whitespace() {
+        let mut guesser = wordle_solver::algorithms::Naive::new();
+        w.play(answer, guesser);
+    }
 }
